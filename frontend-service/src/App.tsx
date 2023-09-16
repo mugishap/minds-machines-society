1011ff5b56ff7f74a07231e38d89304b47fb64a0
mugishap <precieuxmugisha@gmail.com>
2023-09-16 10:31:07 +0200

    [feat]: Change suspense loader

--- frontend-service/src/App.tsx
+++ frontend-service/src/App.tsx
@@ -1,25 +1,17 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 import Pages from './Pages'
-import { motion } from 'framer-motion'
-import { Logo } from './assets'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify'
 
 const App: React.FC = () => {
 
   return (
     <React.Suspense
       fallback={
-        <div className='w-full bg-slate-200 h-screen flex justify-center items-center'>
+        <div className='w-full h-screen flex justify-center items-center'>
           <div className='flex flex-col items-center justify-center'>
-            <div className='relative z-[2] h-44 w-44'>
-              <div className='z-[3] animate-spin absolute w-full h-full rounded-full border-b-2 border-green-600 p-10'>
-              </div>
-              <div className='z-[4] absolute w-full h-full flex items-center justify-center'>
-                <img className='w-20' src={Logo} alt="Logo" />
-              </div>
-            </div>
             <span className='font-bold text-2xl mt-4'>Loading...</span>
           </div>
         </div>
